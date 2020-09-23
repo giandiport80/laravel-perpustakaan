013f7cbc41ebc78e1d2d43ace4bd7de711553eb3
giannurwana19 <giannurwana19@gmail.com>
2020-09-23 21:02:55 +0700

    memperbaiki sidebar #14

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -13,7 +13,7 @@
             <!-- sidebar menu -->
             <ul class="nav sidebar-inner" id="sidebar-menu">
 
-                <li class="has-sub active">
+                <li class="has-sub @if(request()->is('home')) active @endif">
                     <a class="sidenav-item-link" href="{{ route('home') }}">
                         <i class="mdi mdi-image-filter-none"></i>
                         <span class="nav-text">Dashboard</span>
@@ -26,28 +26,29 @@
                         <i class="mdi mdi-view-dashboard-outline"></i>
                         <span class="nav-text">Master Data</span> <b class="caret"></b>
                     </a>
-                    <ul class="collapse" id="dashboard" data-parent="#sidebar-menu">
+                    <ul class="collapse show"
+                        id="dashboard" data-parent="#sidebar-menu">
                         <div class="sub-menu">
 
-                            <li class="active">
+                            <li @if(request()->is('kategori')) class="active" @endif>
                                 <a class="sidenav-item-link" href="{{ route('kategori.index') }}">
                                     <span class="nav-text">Kategori</span>
                                 </a>
                             </li>
 
-                            <li class="active">
+                            <li @if(request()->is('buku')) class="active" @endif>
                                 <a class="sidenav-item-link" href="{{ route('buku.index') }}">
                                     <span class="nav-text">Buku</span>
                                 </a>
                             </li>
 
-                            <li class="active">
+                            <li @if(request()->is('pinjam')) class="active" @endif>
                                 <a class="sidenav-item-link" href="{{ route('pinjam.index') }}">
                                     <span class="nav-text">Peminjaman</span>
                                 </a>
                             </li>
 
-                            <li class="active">
+                            <li @if(request()->is('pengembalian')) class="active" @endif>
                                 <a class="sidenav-item-link" href="{{ route('pengembalian.index') }}">
                                     <span class="nav-text">Pengembalian</span>
                                 </a>
